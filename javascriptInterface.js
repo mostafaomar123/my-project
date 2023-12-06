8167c3fc132c277560deb582b3adb430edce0a54
mostafaomar <mostafa@apple.com>
2023-12-06 14:35:16 +0200

    ADD form validation

--- javascriptInterface.js
+++ javascriptInterface.js
@@ -1,12 +1,14 @@
 document.getElementById("register").onsubmit = function () {
     let user = document.getElementById("user").value;
-    let password = document.getElementById("pass");
-    let passReg = /\d{5}/ig
-    let userReg = /\w+@\w+.\w+/ig
-    let validationPassword = passReg.test(password)
-    let validationEmail = userReg.test(user)
-    if ((validationEmail === false) & validationPassword === false) {
-    return false;
+    let password = document.getElementById("pass").value;
+    let userReg = /\w+@\w+.\w+/ig;
+    let passReg = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/ig;
+    let validationEmail = userReg.test(user);
+    let validationPassword = passReg.test(password);
+    console.log(validationEmail)
+    console.log(validationPassword);
+    if (validationEmail === true && validationPassword === true) {
+    return true;
     }
-    return true
+    return false;
 };
